ad10ab4218f2cd23f4b485500d8b3993f06d9736
jaymompean <jaymompean@gmail.com>
2019-01-09 17:43:55 -0800

    added server component

--- second-app/src/app/server/server.component.ts
+++ second-app/src/app/server/server.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 
-@Component()
+@Component({
+  selector: "app--server",
+  templateUrl: './server.component.html'
+})
 export class ServerComponent {
 
 }
