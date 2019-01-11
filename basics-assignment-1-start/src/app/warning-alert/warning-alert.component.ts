8e1cdf3f86437a0434c4153f57468837aa2ac054
jaymompean <jaymompean@gmail.com>
2019-01-10 17:17:33 -0800

    added semi-complete basics-assignment-1-start (I got interrupted ... got a new job! Yay Jay Woot - Yeaaaahhh!!). The npm install did not work. I think the setup configuration is funky. Will fix later.. [[NOTE: fix later]]

---
+++ basics-assignment-1-start/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular-core';
+
+@Component({
+  selector: 'warning-alert',
+  templateUrl: './warning-alert.component.html',
+  styles: [`
+    h3 {
+      color: red;
+      font-weight: bold;
+    }
+  `]
+})
+
+export class WarningComponent {
+
+}
