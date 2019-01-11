b5c6b1d1cb1ff31a68d9dfcefa0159aa0ebe27da
Jacob Mompean <Jacob@sewingmachinesplus.com>
2019-01-11 15:12:54 -0800

    added hero and message services, and messages component

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +10,27 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
+  selectedHero: Hero;
+
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
 
-  constructor() { }
-
-  ngOnInit() {
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
 }
