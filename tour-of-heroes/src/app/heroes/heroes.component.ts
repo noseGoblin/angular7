55bac973ecf839d4147d3677642aa75c82473e16
Jacob Mompean <Jacob@sewingmachinesplus.com>
2019-01-14 14:30:35 -0800

    added routing module and dashboard component, also created the nav links and hooks into each dashboard view and urls that target hero id values for hero-detail comoonent

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -9,9 +9,6 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  selectedHero: Hero;
-
   heroes: Hero[];
 
   constructor(private heroService: HeroService) { }
@@ -19,15 +16,6 @@ export class HeroesComponent implements OnInit {
   ngOnInit() {
     this.getHeroes();
   }
-  
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
