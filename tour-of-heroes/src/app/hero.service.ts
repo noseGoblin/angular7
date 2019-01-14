4e035d1a9496cdad61f3444ae0ac1f00adc1fada
Jacob Mompean <Jacob@sewingmachinesplus.com>
2019-01-14 15:52:37 -0800

    added HttpClient for angular to create mock server

--- tour-of-heroes/src/app/hero.service.ts
+++ tour-of-heroes/src/app/hero.service.ts
@@ -5,16 +5,20 @@ import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({  providedIn: 'root' })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
-  getHeroes(): Observable<Hero[]> {
+  /** GET heroes from the server */
+  getHeroes (): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    // this.messageService.add('HeroService: fetched heroes');
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
   getHero(id: number): Observable<Hero> {
@@ -22,4 +26,10 @@ export class HeroService {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
   }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+  private heroesUrl = 'api/heroes'; // URL to web api
 }
