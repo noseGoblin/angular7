541d3c36fc7e61e8329fc0ccb6a450aaf634e683
Jacob Mompean <Jacob@sewingmachinesplus.com>
2019-01-15 12:47:18 -0800

    imported  WarningAlertComponent & SuccessAlertCompoonent to app.module.ts

--- basics-assignment-1-start/src/app/success-alert/success-alert.component.ts
+++ basics-assignment-1-start/src/app/success-alert/success-alert.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular-core';
 
 @Component({
-  selector: 'success-alert',
+  selector: 'app-success-alert',
   templateUrl: './success-alert.component.html',
   styles: [`
     h3 {
--- basics-assignment-1-start/src/app/warning-alert/warning-alert.component.ts
+++ basics-assignment-1-start/src/app/warning-alert/warning-alert.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular-core';
 
 @Component({
-  selector: 'warning-alert',
+  selector: 'app-warning-alert',
   templateUrl: './warning-alert.component.html',
   styles: [`
     h3 {
@@ -11,6 +11,6 @@ import { Component } from '@angular-core';
   `]
 })
 
-export class WarningComponent {
+export class WarningAlertComponent {
 
 }
