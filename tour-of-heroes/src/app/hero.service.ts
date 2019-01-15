6d281d7f6ddbee76ba1f7612df405e4e4253872a
Jacob Mompean <Jacob@sewingmachinesplus.com>
2019-01-14 16:59:50 -0800

    added error reporting service that reports errors to the console and spits user friendly message to, while returning a safe value to the app so it can keep working.

--- tour-of-heroes/src/app/hero.service.ts
+++ tour-of-heroes/src/app/hero.service.ts
@@ -6,6 +6,7 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({  providedIn: 'root' })
 export class HeroService {
@@ -14,11 +15,33 @@ export class HeroService {
     private http: HttpClient,
     private messageService: MessageService) { }
 
+    /**
+     * Handle Http operation that failed.
+     * Let the app continue.
+     * @param operation - name of the operation that failed
+     * @param result - optional value to return as the observable result
+    */
+   private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of (result as T);
+    }
+   };
+
   /** GET heroes from the server */
   getHeroes (): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
     // this.messageService.add('HeroService: fetched heroes');
-    return this.http.get<Hero[]>(this.heroesUrl)
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      catchError(this.handleError('getHeroes', []))
+    );
   }
 
   getHero(id: number): Observable<Hero> {
--- tour-of-heroes/src/app/in-memory-data.service.ts
+++ tour-of-heroes/src/app/in-memory-data.service.ts
@@ -1,11 +1,11 @@
-import { InMemoryDataService } from 'angular-in-memory-web-api';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { Hero } from './hero';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class InMemoryDataService {
+export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
       { id: 11, name: 'Mr. Nice' },
