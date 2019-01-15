4a1a774a14445ec58a4b796b5616eb648afb073d
Jacob Mompean <Jacob@sewingmachinesplus.com>
2019-01-14 17:28:42 -0800

    tap into Observable with hero.service.ts

--- tour-of-heroes/src/app/hero.service.ts
+++ tour-of-heroes/src/app/hero.service.ts
@@ -44,10 +44,17 @@ export class HeroService {
     );
   }
 
+  /** GET hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+
     // TODO: send the message _after_ fetching the hero
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    // this.messageService.add(`HeroService: fetched hero id=${id}`);
+    // return of(HEROES.find(hero => hero.id === id));
   }
 
   /** Log a HeroService message with the MessageService */
