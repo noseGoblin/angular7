57037e70c0a09d24e7476adf3979627ea7c170a8
Jacob Mompean <Jacob@sewingmachinesplus.com>
2019-01-16 16:21:35 -0800

    commit of /basics-assignment-3-start

--- tour-of-heroes/src/app/hero-search/hero-search.component.ts
+++ tour-of-heroes/src/app/hero-search/hero-search.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable, Subject } from 'rxjs';
+
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-hero-search',
   templateUrl: './hero-search.component.html',
   styleUrls: ['./hero-search.component.css']
 })
 export class HeroSearchComponent implements OnInit {
+  heores$: Observable<Hero[]>;
+  private searchTerms = new Subject<string>();
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
-  ngOnInit() {
+  // Push a search term in the observable stream.
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  ngOnInit(): void {
+    this.
   }
 
 }
